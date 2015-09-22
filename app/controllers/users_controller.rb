7541f95193be4ce4b2a6583a00951b8bd8598349
phonghoang@gmail.com <phonghoang@gmail.com>
2015-09-21 23:54:47 -0700

    RailsAuth setup, working on sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	redirect_to @user
+  end
+
+  def show
+  	@user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  root to: "welcome#index"
+
+  get "/login", to: "sessions#new"
+
+  post "/sessions", to: "sessions#create"
+
+  get "/sign_up", to: "users#new", as: "sign_up"
+
+  resources :users
+
+end
